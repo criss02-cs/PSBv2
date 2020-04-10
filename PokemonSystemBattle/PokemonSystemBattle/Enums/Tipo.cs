fcf7911c2698aef0a7b6c05e8a98ab14ac991714
criss02-cs <aloigi.cristiano@franchettisalviani.net>
2020-04-10 20:23:12 +0200

    Added the type enum

---
+++ PokemonSystemBattle/PokemonSystemBattle/Enums/Tipo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonSystemBattle
+{
+    public enum Tipo
+    {
+        NORMALE,
+        LOTTA,
+        VOLANTE,
+        VELENO,
+        TERRA,
+        ROCCIA,
+        COLEOTTERO,
+        SPETTRO,
+        ACCIAIO,
+        FUOCO,
+        ACQUA,
+        ERBA,
+        ELETTRO,
+        PSICO,
+        GHIACCIO,
+        DRAGO,
+        BUIO,
+        FOLLETTO
+    }
+}
