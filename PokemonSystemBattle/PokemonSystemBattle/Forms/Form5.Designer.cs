94b3ceb62b4c469e158a26d31082dd6e2ffbfdf5
criss02-cs <aloigi.cristiano@franchettisalviani.net>
2020-04-13 12:42:48 +0200

    Started the possibility of change the user pokemon

---
+++ PokemonSystemBattle/PokemonSystemBattle/Forms/Form5.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace PokemonSystemBattle.Forms
+{
+    partial class Form5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPrimo = new System.Windows.Forms.Button();
+            this.btnSecondo = new System.Windows.Forms.Button();
+            this.btnTerzo = new System.Windows.Forms.Button();
+            this.btnQuarto = new System.Windows.Forms.Button();
+            this.btnQuinto = new System.Windows.Forms.Button();
+            this.btnSesto = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.progressBar3 = new System.Windows.Forms.ProgressBar();
+            this.progressBar4 = new System.Windows.Forms.ProgressBar();
+            this.progressBar5 = new System.Windows.Forms.ProgressBar();
+            this.progressBar6 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // btnPrimo
+            // 
+            this.btnPrimo.Location = new System.Drawing.Point(13, 13);
+            this.btnPrimo.Name = "btnPrimo";
+            this.btnPrimo.Size = new System.Drawing.Size(75, 23);
+            this.btnPrimo.TabIndex = 0;
+            this.btnPrimo.Text = "button1";
+            this.btnPrimo.UseVisualStyleBackColor = true;
+            // 
+            // btnSecondo
+            // 
+            this.btnSecondo.Location = new System.Drawing.Point(13, 60);
+            this.btnSecondo.Name = "btnSecondo";
+            this.btnSecondo.Size = new System.Drawing.Size(75, 23);
+            this.btnSecondo.TabIndex = 1;
+            this.btnSecondo.Text = "button1";
+            this.btnSecondo.UseVisualStyleBackColor = true;
+            // 
+            // btnTerzo
+            // 
+            this.btnTerzo.Location = new System.Drawing.Point(13, 105);
+            this.btnTerzo.Name = "btnTerzo";
+            this.btnTerzo.Size = new System.Drawing.Size(75, 23);
+            this.btnTerzo.TabIndex = 2;
+            this.btnTerzo.Text = "button1";
+            this.btnTerzo.UseVisualStyleBackColor = true;
+            // 
+            // btnQuarto
+            // 
+            this.btnQuarto.Location = new System.Drawing.Point(13, 152);
+            this.btnQuarto.Name = "btnQuarto";
+            this.btnQuarto.Size = new System.Drawing.Size(75, 23);
+            this.btnQuarto.TabIndex = 3;
+            this.btnQuarto.Text = "button1";
+            this.btnQuarto.UseVisualStyleBackColor = true;
+            // 
+            // btnQuinto
+            // 
+            this.btnQuinto.Location = new System.Drawing.Point(13, 199);
+            this.btnQuinto.Name = "btnQuinto";
+            this.btnQuinto.Size = new System.Drawing.Size(75, 23);
+            this.btnQuinto.TabIndex = 4;
+            this.btnQuinto.Text = "button1";
+            this.btnQuinto.UseVisualStyleBackColor = true;
+            // 
+            // btnSesto
+            // 
+            this.btnSesto.Location = new System.Drawing.Point(13, 245);
+            this.btnSesto.Name = "btnSesto";
+            this.btnSesto.Size = new System.Drawing.Size(75, 23);
+            this.btnSesto.TabIndex = 5;
+            this.btnSesto.Text = "button1";
+            this.btnSesto.UseVisualStyleBackColor = true;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(139, 13);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(148, 23);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(139, 61);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(148, 23);
+            this.progressBar2.TabIndex = 7;
+            // 
+            // progressBar3
+            // 
+            this.progressBar3.Location = new System.Drawing.Point(139, 106);
+            this.progressBar3.Name = "progressBar3";
+            this.progressBar3.Size = new System.Drawing.Size(148, 23);
+            this.progressBar3.TabIndex = 8;
+            // 
+            // progressBar4
+            // 
+            this.progressBar4.Location = new System.Drawing.Point(139, 153);
+            this.progressBar4.Name = "progressBar4";
+            this.progressBar4.Size = new System.Drawing.Size(148, 23);
+            this.progressBar4.TabIndex = 9;
+            // 
+            // progressBar5
+            // 
+            this.progressBar5.Location = new System.Drawing.Point(139, 200);
+            this.progressBar5.Name = "progressBar5";
+            this.progressBar5.Size = new System.Drawing.Size(148, 23);
+            this.progressBar5.TabIndex = 10;
+            // 
+            // progressBar6
+            // 
+            this.progressBar6.Location = new System.Drawing.Point(139, 246);
+            this.progressBar6.Name = "progressBar6";
+            this.progressBar6.Size = new System.Drawing.Size(148, 23);
+            this.progressBar6.TabIndex = 11;
+            // 
+            // Form5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(299, 277);
+            this.Controls.Add(this.progressBar6);
+            this.Controls.Add(this.progressBar5);
+            this.Controls.Add(this.progressBar4);
+            this.Controls.Add(this.progressBar3);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnSesto);
+            this.Controls.Add(this.btnQuinto);
+            this.Controls.Add(this.btnQuarto);
+            this.Controls.Add(this.btnTerzo);
+            this.Controls.Add(this.btnSecondo);
+            this.Controls.Add(this.btnPrimo);
+            this.Name = "Form5";
+            this.Text = "Scegli il pokemon da mandare";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPrimo;
+        private System.Windows.Forms.Button btnSecondo;
+        private System.Windows.Forms.Button btnTerzo;
+        private System.Windows.Forms.Button btnQuarto;
+        private System.Windows.Forms.Button btnQuinto;
+        private System.Windows.Forms.Button btnSesto;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.ProgressBar progressBar3;
+        private System.Windows.Forms.ProgressBar progressBar4;
+        private System.Windows.Forms.ProgressBar progressBar5;
+        private System.Windows.Forms.ProgressBar progressBar6;
+    }
+}
---
+++ PokemonSystemBattle/PokemonSystemBattle/Forms/Form5.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PokemonSystemBattle.Forms
+{
+    public partial class Form5 : Form
+    {
+        private List<Pokemon> squadra;
+        public Form5(List<Pokemon> squadra)
+        {
+            InitializeComponent();
+            this.squadra = squadra;
+            try
+            {
+                btnPrimo.Text = squadra[0].Nome;
+                progressBar1.Value = squadra[0].PuntiSalute;
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                
+            }
+        }
+    }
+}
